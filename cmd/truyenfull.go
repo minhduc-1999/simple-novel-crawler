f84f33078d804a8d483b89c09e636c68cab36f40
Duc Truong <minhduc9111999@gmail.com>
2024-07-19 23:05:28 +0700

    add batch crawling

---
+++ cmd/truyenfull.go
@@ -0,0 +1,82 @@
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"sync"
+
+	"github.com/gocolly/colly"
+)
+
+func Execute(fileName, novelName string, total, batchSize int) {
+	var wg sync.WaitGroup
+	nWorker := total/batchSize + 1
+	dir := fmt.Sprintf("./target/%v", novelName)
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		err := os.Mkdir(dir, os.ModePerm)
+		if err != nil {
+			os.Exit(1)
+		}
+	}
+	for i := 0; i < nWorker; i++ {
+		wg.Add(1)
+		go func() {
+			start := i*batchSize + 1
+			end := start + batchSize - 1
+			workerName := fmt.Sprintf("Crawler %d-%d", start, end)
+			if end > total {
+				end = total
+			}
+			file, err := os.Create(fmt.Sprintf("%v/%v_%d-%d.txt", dir, fileName, start, end))
+			if err != nil {
+				log.Printf("[%v] Fail to create file %v\n", workerName, err)
+				wg.Done()
+				return
+			}
+			w := bufio.NewWriter(file)
+
+			c := colly.NewCollector(
+				colly.UserAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"),
+			)
+
+			c.OnHTML("#chapter-c", func(h *colly.HTMLElement) {
+				// Replace <br> by new line
+				h.DOM.Find("br").ReplaceWithHtml("<span>\n</span>")
+				chapName := strings.Split(h.Request.URL.Path, "/")[2]
+				_, err = w.WriteString(chapName + "\n" + h.DOM.Text() + "\n")
+				if err != nil {
+					log.Printf("[%v] Fail to write to file for %v", workerName, h.Request.URL.String())
+				}
+				w.Flush()
+			})
+
+			c.OnHTML("#next_chap", func(h *colly.HTMLElement) {
+				if strings.Contains(h.Attr("class"), "disabled") {
+					// No more chapter
+					return
+				}
+				if start > end {
+					return
+				}
+				nextChapUrl := h.Attr("href")
+				c.Visit(nextChapUrl)
+			})
+
+			c.OnResponse(func(r *colly.Response) {
+				start += 1
+			})
+
+			c.OnError(func(r *colly.Response, err error) {
+				log.Printf("[%v] Error: %v\n", workerName, r.Request.URL.String())
+			})
+
+			c.Visit(fmt.Sprintf("https://truyenfull.vn/%v/chuong-%d/", novelName, start))
+			log.Printf("[%v] Succeed\n", workerName)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
--- main.go
+++ main.go
@@ -1,75 +1,28 @@
 package main
 
 import (
-	"bufio"
+	"crawler/cmd"
 	"log"
 	"os"
 	"strconv"
-	"strings"
-
-	"github.com/gocolly/colly/v2"
 )
 
 func main() {
-	if len(os.Args) != 3 {
+	if len(os.Args) != 5 {
 		log.Println("Please enter options")
 		os.Exit(1)
 	}
-
+	novelName := os.Args[1]
 	fileName := os.Args[2]
-	max, err := strconv.Atoi(os.Args[1])
+	total, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		log.Println("Final chapter required")
+		os.Exit(1)
+	}
+	batchSize, err := strconv.Atoi(os.Args[4])
 	if err != nil {
 		log.Println("error max")
 		os.Exit(1)
 	}
-	cur := 0
-	f, err := os.Create("./target/" + fileName)
-	if err != nil {
-		log.Println("Fail to create file")
-		os.Exit(1)
-	}
-	w := bufio.NewWriter(f)
-
-	c := colly.NewCollector(
-		colly.UserAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"),
-	)
-
-	c.OnHTML("#chapter-c", func(h *colly.HTMLElement) {
-		// Replace <br> by new line
-		h.DOM.Find("br").ReplaceWithHtml("<span>\n</span>")
-		chapName := strings.Split(h.Request.URL.Path, "/")[2]
-		_, err = w.WriteString(chapName + "\n" + h.DOM.Text() + "\n")
-		if err != nil {
-			log.Printf("Fail to write to file for %v", h.Request.URL.String())
-			os.Exit(1)
-		}
-		w.Flush()
-	})
-
-	c.OnHTML("#next_chap", func(h *colly.HTMLElement) {
-		if strings.Contains(h.Attr("class"), "disabled") {
-			// No more chapter
-			return
-		}
-		if cur > max {
-			return
-		}
-		nextChapUrl := h.Attr("href")
-		c.Visit(nextChapUrl)
-	})
-
-	c.OnResponse(func(r *colly.Response) {
-		log.Printf("Success: %v\n", r.Request.URL.String())
-		cur += 1
-	})
-
-	c.OnError(func(r *colly.Response, err error) {
-		log.Printf("Error: %v\n", r.Request.URL.String())
-	})
-
-	c.OnRequest(func(r *colly.Request) {
-		log.Printf("Visiting %v\n", r.URL.String())
-	})
-
-	c.Visit("https://truyenfull.vn/gia-thien/chuong-1/")
+	cmd.Execute(fileName, novelName, total, batchSize)
 }
